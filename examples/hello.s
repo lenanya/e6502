65716a392975ebdeb1c25f3b42efa76ac84e4f56
lenanya <lenashimizu@gmx.net>
2025-12-27 21:05:52 +0000

    properly comment the example

--- examples/hello.s
+++ examples/hello.s
@@ -1,36 +1,55 @@
+ ;; make sure vasm knows
+ ;; this will be at 0x8000
+ ;; in the emulator
  .org $8000
+ ;; write address of hello_str 
+ ;; to 0x00 and 0x01
  LDA #<hello_str
  STA $00
  LDA #>hello_str
  STA $01
+ ;; load zp address of 
+ ;; the address we just stored
  LDA #$00
  JSR puts
  BRK
 
-;; A -> char
+ ;; write a char to stdout
+ ;; A -> char
 putc:
  ;; writing to 0x4000 causes 
  ;; the emulator to print
  ;; the byte written
  STA $4000
  RTS
-
-;; A -> zpg -> *str (null terminated)
+ 
+ ;; write a string to stdout
+ ;; A -> zpg -> *str (null terminated)
 puts:
  LDY #$00
 putsloop:
+ ;; get character
  LDA ($00), Y
+ ;; go to next char
  INY
+ ;; is A 0?
+ ;; (end of string)
  CMP #$00
+ ;; if yes, we're done
  BEQ done
+ ;; else we print the char
  JSR putc
+ ;; loop
  JMP putsloop
 done:
  RTS
  
  hello_str:
  .asc "Hello World!"
+ ;; newline and null terminator
  .byte 0x10, 0
+ ;; Reset Vector
  .org $fffc
  .word $8000
+ ;; pad to 32KiB
  .word $0000
